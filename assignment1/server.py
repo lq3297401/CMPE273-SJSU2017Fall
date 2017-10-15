e2c4f0fd96e266de6505cf8f9ca5ae8bc076d4df
Qi Liu <liuqi0087@gmail.com>
2017-10-15 16:37:43 -0700

    Update rocksDB, but now can’t get the foo.py document

--- assignment1/server.py
+++ assignment1/server.py
@@ -1,6 +1,10 @@
 # curl http://localhost:8000/api/v1/scripts -d "data=It's my number" -X POST
 # curl http://localhost:8000/api/v1/scripts/123456 -X GET
 
+# curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@/tmp/foo.py" http://localhost:8000/api/v1/scripts
+
+import rocksdb
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
@@ -10,10 +14,16 @@ api = Api(app)
 scripts = {}
 
 class ScriptUploader(Resource):
+    def __init__(self):
+        self.db = rocksdb.DB("assignment1.db", rocksdb.Options(create_if_missing=True))
+
     def post(self):
+        print("put data in rocksDB")
         s_id="123456"
         scripts[s_id] = request.form['data']
-        return {"script-id": s_id}
+        self.db.put(s_id, scripts[s_id])
+        return self.db.get(s_id)
+        # return {"script-id": s_id }
 
 class ScriptDownloader(Resource):
     def get(self, script_id):
