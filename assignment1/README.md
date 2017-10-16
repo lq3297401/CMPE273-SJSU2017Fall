4917ab583777794e4042121517c2bac2d2de1fe1
Qi Liu <liuqi0087@gmail.com>
2017-10-15 17:40:52 -0700

    Update random number as s_id
    TODO: read foo.py from “data=@/$(pwd)/foo.py” POST request

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,6 +1,12 @@
 1. Install Flask-RESTful with pip(Flask-RESTful requires Python version 2.6, 2.7, 3.3, or 3.4.):
     pip install flask-restful
-2. Run server in 1st terminal:
+
+2. Install rocksDB, python-rocksdb:
+  brew install rocksdb
+  sudo pip install python-rocksdb
+
+3. Run server in 1st terminal:
     python server.py
-3. Run "curl request" in 2nd terminal:
+    
+4. Run "curl request" in 2nd terminal:
     curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@/tmp/foo.py" http://localhost:8000/api/v1/scripts
--- assignment1/server.py
+++ assignment1/server.py
@@ -1,33 +1,40 @@
 # curl http://localhost:8000/api/v1/scripts -d "data=It's my number" -X POST
 # curl http://localhost:8000/api/v1/scripts/123456 -X GET
 
+# curl http://localhost:8000/api/v1/scripts -d "data=@/$(pwd)/foo.py" -X POST
+
 # curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@/tmp/foo.py" http://localhost:8000/api/v1/scripts
 
 import rocksdb
 
 from flask import Flask, request
 from flask_restful import Resource, Api
+from random import *
 
 app = Flask(__name__)
 api = Api(app)
 
 scripts = {}
 
-class ScriptUploader(Resource):
-    def __init__(self):
-        self.db = rocksdb.DB("assignment1.db", rocksdb.Options(create_if_missing=True))
+class GlobalVar(Resource):
+    # def __init__(self):
+        db = rocksdb.DB("assignment1.db", rocksdb.Options(create_if_missing=True))
 
+class ScriptUploader(Resource):
     def post(self):
-        print("put data in rocksDB")
-        s_id="123456"
+        print("201 Created")
+        x = randint(100000, 999999)    # Pick a random number between 100000 and 999999.
+        s_id = str (x)
         scripts[s_id] = request.form['data']
-        self.db.put(s_id, scripts[s_id])
-        return self.db.get(s_id)
+        GlobalVar.db.put(s_id, scripts[s_id])
+        return {s_id:GlobalVar.db.get(s_id)}
         # return {"script-id": s_id }
 
 class ScriptDownloader(Resource):
     def get(self, script_id):
-        return {script_id: scripts[script_id]}
+        print("get data from rocksDB")
+        # return {script_id: scripts[script_id]}
+        return GlobalVar.db.get(script_id)
 
 api.add_resource(ScriptUploader, '/api/v1/scripts')
 api.add_resource(ScriptDownloader, '/api/v1/scripts/<string:script_id>')
