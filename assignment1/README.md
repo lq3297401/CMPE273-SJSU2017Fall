18a10a3cffc9bdc0d90c767e01f55e71dfaaa9b0
Qi Liu <liuqi0087@gmail.com>
2017-10-17 04:26:50 -0700

    Finally finish POST and GET successfully. Yeah~

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,3 +1,5 @@
+Self note:
+
 1. Install Flask-RESTful with pip(Flask-RESTful requires Python version 2.6, 2.7, 3.3, or 3.4.):
     pip install flask-restful
 
@@ -9,4 +11,8 @@
     python server.py
 
 4. Run "curl request" in 2nd terminal:
-    curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@/tmp/foo.py" http://localhost:8000/api/v1/scripts
+    curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@/$(pwd)/foo.py" http://localhost:8000/api/v1/scripts
+    //this request will generate a random number as script_id
+
+    curl -i http://localhost:8000/api/v1/scripts/<script_id>
+    // replace the <script_id> as the one in output above
--- assignment1/server.py
+++ assignment1/server.py
@@ -1,50 +1,76 @@
-# curl http://localhost:8000/api/v1/scripts/123456 -X GET
-
-# curl http://localhost:8000/api/v1/scripts -d "data=@/$(pwd)/foo.py" -X POST
-
-# curl -i -X POST -H "Content-Type: multipart/form-data" -F "data=@/$(pwd)/foo.py" http://localhost:8000/api/v1/scripts
-#curl -i http://localhost:8000/api/v1/scripts/123456
-
--F是直接把file传过来，server 拿到file以后先存到server 的file system,记住存的地址，然后把本地的这个地址配合ID保存进rocksdb
-
-读取时，用id找到本地的地址，从地址拿到文件，ececute  exec(open(foo.py)).read()，然后返回execute的结果
-
-import rocksdb
-
-from flask import Flask, request
-from flask_restful import Resource, Api
+# reference: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+#            http://pythonexample.com/code/flask-file-upload-example/
+import os
+from os import path
+from flask import Flask, request, redirect, url_for, send_from_directory, jsonify
+from werkzeug.utils import secure_filename
 from random import *
+import rocksdb
+import sys
+import StringIO
+import contextlib
+
+
+UPLOAD_FOLDER = '/Users/qi/Desktop/273Git/assignment1/upload/'  #the place that file save
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'py', 'gif'])
 
 app = Flask(__name__)
-api = Api(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-scripts = {}
 
-class GlobalVar(Resource):
-    # def __init__(self):
-        db = rocksdb.DB("assignment1.db", rocksdb.Options(create_if_missing=True))
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-class ScriptUploader(Resource):
-    def post(self):
-        print("201 Created")
-        x = randint(100000, 999999)    # Pick a random number between 100000 and 999999.
-        s_id = str (x)
-        #TODO save foo.py to rocksdb value by flask head handler
-        scripts[s_id] = request.form['data']
-        GlobalVar.db.put(s_id, scripts[s_id])
-        return {s_id:scripts[s_id]}
 
-        # return {"script-id": s_id }
+@app.route('/api/v1/scripts', methods=['POST'])
+def upload_file():
+    x = randint(100000, 999999)    # Pick a random number between 100000 and 999999 as s_id.
+    s_id = str (x)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'data' not in request.files:  # the request use "data" to passing file address
+            print 'no file'
+            return redirect(request.url)
+        file = request.files['data']  # the request use "data" to passing file address
+        # if user does not select file, browser also submit a empty part without filename
+        if file.filename == '':
+            print 'no filename'
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_address = str(os.path.join(app.config['UPLOAD_FOLDER'], filename)) #/Users/qi/Desktop/273Git/assignment1/upload/foo.py
+            db.put(s_id, file_address)
+            result = {'script-id':s_id}
+            print '201 Created\n'
+            return jsonify(result)
 
-class ScriptDownloader(Resource):
-    def get(self, script_id):
-        print("200 OK")
-        # return {script_id: scripts[script_id]}
-        return GlobalVar.db.get(script_id)
 
-api.add_resource(ScriptUploader, '/api/v1/scripts')
-api.add_resource(ScriptDownloader, '/api/v1/scripts/<string:script_id>')
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
-if __name__ == '__main__':
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO.StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+
+@app.route('/api/v1/scripts/<script_id>', methods=['GET'])
+def get_script(script_id):
+    fileDir = db.get(script_id)
+    print '200 OK'
+    with stdoutIO() as s:
+        exec(open(fileDir).read())
+    return s.getvalue()
+
+
+if __name__ == "__main__":
+    db = rocksdb.DB("assignment1.db", rocksdb.Options(create_if_missing=True))
     app.run(port=8000)
     app.run(debug=True)
