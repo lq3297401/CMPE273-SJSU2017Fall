b826c7965285190f50688a332859b1d88827e18a
Qi Liu <liuqi0087@gmail.com>
2017-11-18 23:17:01 -0800

    Update server.py and datastore.proto

--- assignment2/server.py
+++ assignment2/server.py
@@ -16,27 +16,44 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class MyDatastoreServicer(datastore_pb2.DatastoreServicer):
     def __init__(self):
-        self.db = rocksdb.DB("lab1.db", rocksdb.Options(create_if_missing=True))
+        self.db = rocksdb.DB("database/assignment2.db", rocksdb.Options(create_if_missing=True))
+        self.connectionCount = 0
+        print("Server Ready!")
 
+    def copyProcess(self, request, context):
+        '''
+        start server and client can start copy process
+        '''
+        selfId = self.connectionCount;
+        arrDataDict.append([])
+        self.connectionCount =  connectionCount + 1
+        print("Start copy processing: " + str(selfId))
+
+        while True:
+            if len(arrDataDict[selfId]) != 0:
+                data = arrDataDict[selfId].pop(0)
+                yield datastore_pb2.ResponseAll(key=data["key"], value=data["value"])
+
+    def followers:
+        # need def followers
+
+    @followers:
     def put(self, request, context):
         print("put")
-        key = uuid.uuid4().hex
-        # TODO - save key and value into DB converting request.data string to utf-8 bytes
-        self.db.put(key.encode(), request.data.encode())
-        return datastore_pb2.Response(data=key)
+        self.db.put(request.key, request.value)
+        return datastore_pb2.GetRequest01(key=request.key, value=request.value)
 
     def get(self, request, context):
         print("get")
-        # TODO - retrieve the value from DB by the given key. Needs to convert request.data string to utf-8 bytes.
-        value = None
-        value = self.db.get(request.data.encode())
-        return datastore_pb2.Response(data=value)
+        # value = None
+        value = self.db.get(request.key)
+        return datastore_pb2.GetRequest01(key=request.key, value=request.value)
 
 def run(host, port):
     '''
     Run the GRPC server
     '''
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
     datastore_pb2_grpc.add_DatastoreServicer_to_server(MyDatastoreServicer(), server)
     server.add_insecure_port('%s:%d' % (host, port))
     server.start()
