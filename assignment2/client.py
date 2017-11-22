9996b47303d1d31fb37b2e058586f3af2b786ccb
Qi Liu <liuqi0087@gmail.com>
2017-11-22 08:11:08 -0800

    Update proto and server, client Add slaveRegister part

--- assignment2/client.py
+++ assignment2/client.py
@@ -1,44 +1,43 @@
 '''
 ################################## client.py #############################
-# 
+#
 ################################## client.py #############################
 '''
 import grpc
 import datastore_pb2
 import argparse
+import sys
+import rocksdb
 
 PORT = 3000
 
 class DatastoreClient():
-    
+
     def __init__(self, host='0.0.0.0', port=PORT):
         self.channel = grpc.insecure_channel('%s:%d' % (host, port))
         self.stub = datastore_pb2.DatastoreStub(self.channel)
+        print("Client start running...")
 
-    def put(self, value):
-        return self.stub.put(datastore_pb2.Request(data=value))
+    def put(self, key, data):
+        print("Pushing data to main database...")
+        print("key = " + key + ", data = " + data)
+        return self.stub.put(datastore_pb2.Request(key=key, data=data))
 
     def get(self, key):
-        return self.stub.get(datastore_pb2.Request(data=key))
+        print("Geting data from main database...")
+        return self.stub.get(datastore_pb2.Request(key=key))
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("host", help="display a square of a given number")
-    args = parser.parse_args()
-    print("Client is connecting to Server at {}:{}...".format(args.host, PORT))
-    client = DatastoreClient(host=args.host)
-    
-    value = 'foo'
-    print("## PUT Request: value = " + value) 
-    resp = client.put(value)
-    key = resp.data
-    print("## PUT Response: key = " + key)
+if len(sys.argv) == 3:
+    Key = sys.argv[1]
+    Value = sys.argv[2]
+    print(Key + ",***** " + Value)
+elif len(sys.argv) == 2:
+    Key = sys.argv[1]
+    print(Key + "----- " )
+else:
+    print("Wrong Input. Please follow the input format as follows:\nExample for inserting key-value data: client.py key1 value1 \nExample for getting data from key1: client.py key1")
+    exit()
 
-    print("## GET Request: key = " + key) 
-    resp = client.get(key)
-    print("## GET Response: value = " + resp.data) 
-
-
-if __name__ == "__main__":
-    main()
-
+client = DatastoreClient()
+response = client.put(Key, Value)
+print(response.key, response.data)
--- assignment2/server.py
+++ assignment2/server.py
@@ -9,6 +9,7 @@ import datastore_pb2
 import datastore_pb2_grpc
 import uuid
 import rocksdb
+import sys
 
 from concurrent import futures
 
@@ -16,44 +17,41 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class MyDatastoreServicer(datastore_pb2.DatastoreServicer):
     def __init__(self):
-        self.db = rocksdb.DB("database/assignment2.db", rocksdb.Options(create_if_missing=True))
-        self.connectionCount = 0
-        print("Server Ready!")
+        self.db = rocksdb.DB("server.db", rocksdb.Options(create_if_missing=True))
+        print("-------- Main server start --------")
 
-    def copyProcess(self, request, context):
-        '''
-        start server and client can start copy process
-        '''
-        selfId = self.connectionCount;
-        arrDataDict.append([])
-        self.connectionCount =  connectionCount + 1
-        print("Start copy processing: " + str(selfId))
-
-        while True:
-            if len(arrDataDict[selfId]) != 0:
-                data = arrDataDict[selfId].pop(0)
-                yield datastore_pb2.ResponseAll(key=data["key"], value=data["value"])
-
-    def followers:
-        # need def followers
-
-    @followers:
     def put(self, request, context):
-        print("put")
-        self.db.put(request.key, request.value)
-        return datastore_pb2.GetRequest01(key=request.key, value=request.value)
+        '''
+        put data into RocksDB
+        '''
+        print("-------- Put data into RocksDB --------")
+        print(request)
+
+        self.db.put(request.key, request.data)
+        return datastore_pb2.Response(key=request.key, data=request.data)
 
     def get(self, request, context):
-        print("get")
-        # value = None
+        '''
+        get data from RocksDB
+        '''
+        print("-------- Get data from RocksDB --------")
         value = self.db.get(request.key)
-        return datastore_pb2.GetRequest01(key=request.key, value=request.value)
+        return datastore_pb2.Response(key=request.key, data=value)
+
+    def register(self, request, context):
+        '''
+        get slave register information
+        '''
+        print("-------- Slave server information --------")
+        id = self.db.put(request.id, request.port)
+        print("Register id = " + request.id + ", register port = " + request.port)
+        return datastore_pb2.SlaveRegisterResponse(id=request.id, port=request.port)
 
 def run(host, port):
     '''
     Run the GRPC server
     '''
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     datastore_pb2_grpc.add_DatastoreServicer_to_server(MyDatastoreServicer(), server)
     server.add_insecure_port('%s:%d' % (host, port))
     server.start()
